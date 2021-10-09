d18af35482b95641862736d1a764355a826eed2c
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-10-09 15:33:56 +0000

    implemeted loadTweets Function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,37 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-    // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-
+$(document).ready(function() { 
 const createTweetElement = function (data) {
  let $tweet = ` 
  <article>
@@ -63,8 +33,35 @@ const renderTweets = function(tweets) {
   // loops through tweets
   for (const tweet of tweets) {
     const tweetElement = createTweetElement(tweet);
-    $('#tweet-container').append(tweetElement);
+    $('#tweet-container').prepend(tweetElement);
   }
 };
 
-renderTweets(data);
+$(function() {
+  const $form = $("form");
+
+ $form.on('submit', function (event) {
+  event.preventDefault();
+  console.log("Someone clicked the button");
+
+  const data = $form.serialize();
+  const tweetUrl = "http://localhost:8080/tweets";
+
+  $.ajax({data: data, url: tweetUrl, method: 'POST'})
+   .then(function () {
+    console.log('Success!!')
+    return loadTweets()
+   });
+  });
+ });
+
+ const loadTweets = function () {
+   const tweetUrl = "http://localhost:8080/tweets";
+   $.ajax({url: tweetUrl, method: 'GET'})
+   .then(function(response) {
+     return renderTweets(response);
+   });
+ };
+
+loadTweets();
+})
